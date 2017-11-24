e6f57717f7cfe072879e1e2882101a431f9c2d1e
scottclay <scottclay8@gmail.com>
2017-11-24 15:26:39 +0000

    Don't init DustRates variables if not asked for in MakeFile

--- code/main.c
+++ code/main.c
@@ -559,11 +559,13 @@ int join_galaxies_of_progenitors(int halonr, int ngalstart, int *cenngal)
 	  Gal[ngal].RadioAccretionRate=0.0;
 
 #ifdef DETAILED_DUST
+#ifdef FULL_DUST_RATES
       Gal[ngal].DustISMRates.AGB = 0.0;
 	  Gal[ngal].DustISMRates.SNII = 0.0;
 	  Gal[ngal].DustISMRates.SNIA = 0.0;
 	  Gal[ngal].DustISMRates.GROW = 0.0;
 	  Gal[ngal].DustISMRates.DEST = 0.0;
+#endif
 #endif	  
 	  
 #ifdef GALAXYTREE
